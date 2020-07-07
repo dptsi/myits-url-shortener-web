c34610b0358c55abf586735317dfb5ad1fa75c0d
mohadam83 <moh.adam.83@gmail.com>
2020-07-07 14:13:08 +0700

    add column qr code

--- resources/views/snippets/link_table.blade.php
+++ resources/views/snippets/link_table.blade.php
@@ -5,6 +5,7 @@
             <th>Long Link</th>
             <th>Clicks</th>
             <th>Date</th>
+            <th>QR Code</th>
             @if ($table_id == "admin_links_table")
             {{-- Show action buttons only if admin view --}}
             <th>Creator</th>
