967e9a5dc8fd3a252ebb2f2c3f74450c257704ce
bayulaxana <bynd4spd@gmail.com>
2020-07-16 23:12:50 +0700

    added function to check date expiration

--- app/Helpers/AdminHelper.php
+++ app/Helpers/AdminHelper.php
@@ -1,8 +1,17 @@
 <?php
 namespace App\Helpers;
 
+use DateTime;
+
 class AdminHelper {
     public static function pass() {
         
+    }
+
+    public static function isDateExpired($dateExpired) {
+        $now = new DateTime('now');
+        $expire = new DateTime($dateExpired);
+
+        return $now >= $expire;
     }
 }
