2753366ec16c4d8725aa1e1ad94c4aa797633cfa
Adetiya Bagus Nusantara <nusantara@its.ac.id>
2022-11-17 14:40:26 +0700

    edited back button 404 & 403

--- src/resources/views/errors/403.blade.php
+++ src/resources/views/errors/403.blade.php
@@ -3,5 +3,5 @@
 @section('content')
 <h1>403</h1>
 <p>Anda tidak berhak mengakses halaman ini.</p>
-<a href="{{ url('/') }}" class="btn btn-primary"><span class="fa fa-arrow-left"></span> Kembali</a>
+<a href="https://shortener.its.ac.id" class="btn btn-primary"><span class="fa fa-arrow-left"></span> Kembali</a>
 @endsection
