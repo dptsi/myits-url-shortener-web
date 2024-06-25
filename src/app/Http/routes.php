70d81050d01814c8212561c9e5d82204e3c0e35c
Adetiya Bagus Nusantara <nusantara@its.ac.id>
2024-06-25 15:51:42 +0700

    updated

--- src/app/Http/routes.php
+++ src/app/Http/routes.php
@@ -77,4 +77,4 @@ $app->group(['prefix' => '/api/v2', 'namespace' => 'App\Http\Controllers\Api', '
     $app->post('data/link', ['as' => 'api_link_analytics', 'uses' => 'ApiAnalyticsController@lookupLinkStats']);
 });
 
-$app->get('/generate', ['as' => 'generate', 'uses' => 'GenerateController@index']);
+$app->get('/generate-nusantara-qr', ['as' => 'generate', 'uses' => 'GenerateController@index']);
